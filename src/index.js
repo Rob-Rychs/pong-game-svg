ec9e2b6c8a299a19f2647eee4a4bf414d4eebab8
Rob-Rychs <Robrychel@gmail.com>
2017-08-03 13:21:11 -0700

    added game board, paddle, ball svg

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Game from './partials/Game'
 // create a game instance
 const game = new Game('game', 512, 256);
 
+// iife that 1. immeidiately calls itself and then is recursively calling itself forever in an infinite loop with the global method of requestAnimationFrame() 
 (function gameLoop() {
     game.render();
     requestAnimationFrame(gameLoop);
